25982670d8525fe32e916b8eff8b7838d6053c8f
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-11-17 12:01:31 -0800

    Added FizzBuzz & Programmer

--- src/main/java/com/xyzcorp/javapatterns/factorymethod/example/ConcreteCountryFactory.java
+++ src/main/java/com/xyzcorp/javapatterns/factorymethod/example/ConcreteCountryFactory.java
@@ -4,6 +4,7 @@ public class ConcreteCountryFactory implements CountryFactory {
 
     @Override
     public Country createCountry(String name, byte[] flag, String capital) {
+        //complexity
         return new ConcreteCountry(name, flag, capital);
     }
 }
